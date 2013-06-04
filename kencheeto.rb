35ce7a4cd6eadc68ad1ff82119de0cbcbefbc032
Kenshiro Nakagawa <kencheeto@gmail.com>
2013-06-04 01:46:56 -0700

    homepage + list of top artists

--- kencheeto.rb
+++ kencheeto.rb
@@ -1,12 +1,20 @@
-# load variables into ENV from .env
-require 'dotenv'; Dotenv.load
-
+require 'dotenv'; Dotenv.load # load variables into ENV from .env
 require 'sinatra'
 
-require './lastfm_client'
-require 'json'
-
 get '/' do
-  client = LastFMClient.new ENV['LASTFM_KEY']
-  '<pre><code>' + JSON.pretty_generate(client.loved_tracks) + '</code></p>'
+  erb :index
+end
+
+get '/top_artists' do
+  names = lastfm_client.top_artists.map(&:name)
+  '<ul>' +
+    names.map{|name| '<li>' + name + '</li>'}.join +
+  '</ul>'
+end
+
+private
+
+def lastfm_client
+  require './lastfm_client'
+  @client ||= LastFM::Client.new ENV['LASTFM_KEY']
 end
--- lastfm_client.rb
+++ lastfm_client.rb
@@ -1,41 +1,47 @@
 require 'faraday'
 require 'faraday_middleware'
 
-class LastFMClient
-  def initialize key
-    @key = key
-    @conn = Faraday.new( url: root_url ) do |conn|
-      conn.response :mashify
+module LastFM
+  class Client
+    def initialize key
+      @key = key
+      @conn = Faraday.new( url: root_url ) do |conn|
+        conn.response :mashify
+        conn.response :json, :content_type => /\bjson$/
+        conn.adapter :net_http
+      end
     end
-  end
 
-  def loved_tracks
-    api 'user.getLovedTracks'
-  end
+    def loved_tracks
+      mash = api 'user.getLovedTracks'
+      mash.lovedtracks.track
+    end
 
-  def top_artists
-    api 'user.getTopArtists'
-  end
+    def top_artists
+      mash = api 'user.getTopArtists'
+      mash.topartists.artist
+    end
 
-  private
+    private
 
-  def api method, user: 'kencheeto'
-    response = @conn.get '', defaults.merge({
-      method: method,
-      user: user
-    })
+    def api method, user: 'kencheeto'
+      response = @conn.get '', defaults.merge({
+        method: method,
+        user: user
+        })
 
-    JSON.parse response.body
-  end
+      response.body
+    end
 
-  def defaults
-    @defaults ||= {
-      api_key: @key,
-      format: 'json'
-    }
-  end
+    def defaults
+      @defaults ||= {
+        api_key: @key,
+        format: 'json'
+      }
+    end
 
-  def root_url
-    'http://ws.audioscrobbler.com/2.0'
+    def root_url
+      'http://ws.audioscrobbler.com/2.0'
+    end
   end
 end
