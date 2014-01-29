d51a81921dc82c07c0f6db90fc2be142fd038bd8
Kenshiro Nakagawa <kencheeto@gmail.com>
2014-01-29 02:41:36 -0800

    remove cruft

--- kencheeto.rb
+++ kencheeto.rb
@@ -1,26 +1,12 @@
-require 'dotenv'; Dotenv.load # load variables into ENV from .env
+require 'dotenv'; Dotenv.load
 require 'sinatra'
 require 'sass'
-require './lastfm_client'
 require 'kramdown'
 
 get '/' do
   erb :index
 end
 
-get '/top_artists' do
-  names = lastfm_client.top_artists.map(&:name)
-  '<ul>' +
-    names.map{|name| '<li>' + name + '</li>'}.join +
-  '</ul>'
-end
-
 get '/stylesheet.css' do
   scss :stylesheet
-end
-
-private
-
-def lastfm_client
-  @client ||= LastFM::Client.new ENV['LASTFM_KEY']
 end
